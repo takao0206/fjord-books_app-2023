48c5789612e1c67407bc6264ca9267aecdbb683c
takao0206 <takao.haruna.0206@gmail.com>
2024-10-28 00:24:45 +0900

    機能の追加: 画面上のリンクで英語と日本語を切り替える機能。

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
