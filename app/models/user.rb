82c24c0275cd35751e12c52b977a1beaa94f7f32
takao0206 <takao.haruna.0206@gmail.com>
2024-11-18 21:28:10 +0900

    ユーザーが自分のプロフィールの画像を操作できる機能を追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :avatar
+
+  validate :avatar_format
+
+  private
+
+  def avatar_format
+    return unless avatar.attached?
+
+    allowed_types = ['image/jpg', 'image/png', 'image/gif']
+    return unless avatar.content_type.nil? || !allowed_types.include?(avatar.content_type)
+
+    errors.add(:avatar, :invalid)
+    avatar.purge
+  end
 end
