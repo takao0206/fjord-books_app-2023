9c0804571bafa7009f8271e095b8f6df3d930814
takao0206 <takao.haruna.0206@gmail.com>
2024-12-25 13:00:10 +0900

    コメント作成中にエラーが出た場合、booksとreportsそれぞれの`:show`にレンダリングするように修正。

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Books::CommentsController < CommentsController
+  def create
+    @commentable = find_commentable
+    @comment = @commentable.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      flash[:alert] = @comment.errors.full_messages.first
+      @book = @commentable
+      render 'books/show', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def find_commentable
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,6 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
   before_action :authorize_user!, only: %i[edit update destroy]
 
-  def create
-    @commentable = find_commentable
-    @comment = @commentable.comments.build(comment_params)
-    @comment.user = current_user
-
-    if @comment.save
-      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
   def edit; end
 
   def update
